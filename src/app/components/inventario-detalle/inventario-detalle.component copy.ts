6fd957607a14be4bf4ebfa3c7d1c221631d48784
Juan Toledo <johntoledot@gmail.com>
2021-03-23 19:00:24 -0600

    23032021

---
+++ src/app/components/inventario-detalle/inventario-detalle.component copy.ts
@@ -0,0 +1,384 @@
+import { Component, OnInit } from '@angular/core';
+import { EChartsOption } from 'echarts';
+
+@Component({
+  selector: 'app-inventario-detalle',
+  templateUrl: './inventario-detalle.component.html'
+})
+export class InventarioDetalleComponent implements OnInit {
+
+xAxisData = [];
+data1 = [];
+data2 = [];
+data3 = [];
+data4 = [];
+data5 = [];
+data6 = [];
+data7 = [];
+data8 = [];
+data9 = [];
+data10 = [];
+data11 = [];
+data12 = [];
+data13 = [];
+data14 = [];
+data15 = [];
+data16 = [];
+data17 = [];
+data18 = [];
+data19 = [];
+data20 = [];
+data21 = [];
+
+linea1 = [];
+
+link = [];
+
+colorAzul: string = '#476DC5';
+colorVerde: string = '#3FAE2B';
+colorAmarillo: string = '#FFCC00';
+
+
+emphasisStyle = {
+    itemStyle: {
+        shadowBlur: 10,
+        shadowColor: 'rgba(0,0,0,0.3)',
+        gapWidth: 5
+    }
+};
+
+chartOption: EChartsOption = {
+  legend: {
+    data: ['bar', 'bar2', 'bar3', 'bar4'],
+    // left: '10%'
+  },
+  tooltip: {},
+  xAxis: {
+    data: this.xAxisData,
+    name: '',
+    // axisLine: {onZero: false},
+    // splitLine: {show: false},
+    // splitArea: {show: false}
+    // axisTick: {
+    //   alignWithLabel: true
+    // }
+  },
+  yAxis: [
+    {
+      id:   'AvgAxis',
+      type: 'value',
+      name: 'Inventario',
+      // min: 0,
+      // max: 30,
+      // interval: 5,
+      axisLabel: { 
+        formatter: function (value, index) {
+                  let aux = value/1000000;
+                  return aux + 'M';
+              }
+        }
+    },
+  {
+      id:   'MaxAxis',
+      type: 'value',
+      name: 'Venta',
+      min: 0,
+      max: 1000,
+      interval: 100,
+      axisLabel: { formatter: '{value} K' }
+  }
+  ],
+  // yAxis: [
+  //   {
+  //     axisLabel: {
+  //       formatter: function (value, index) {
+  //         let aux = value/1000000;
+  //         return aux + 'M';
+  //     },
+  //     fontSize: '12'
+  //     }
+  //   },
+  //   {
+  //     type: 'value',
+  //     name: "Wi-Fi\nUsers",
+  //     min: 0,
+  //     max: 500,
+  //     position: 'right',
+  //     offset:90,
+  //     splitLine: {
+  //         show: true
+  //     },
+  //   }
+  // ],
+  // grid: {
+  //   bottom: 100
+  // },
+  dataZoom: [
+    {
+      type: 'inside',
+      xAxisIndex: 0,
+      minSpan: 5,
+      startValue: 0,
+      endValue: 21
+    }, 
+    {
+        type: 'slider',
+        xAxisIndex: 0,
+        minSpan: 5,
+        bottom: 50
+    }
+  ],
+  
+  series: [
+    {
+      name: 'bar',
+      type: 'bar',
+      stack: 'uno',
+      emphasis: this.emphasisStyle,
+      data: this.data1,
+      color: this.colorAzul,
+    },
+    {
+      name: 'bar2',
+      type: 'bar',
+      stack: 'uno',
+      emphasis: this.emphasisStyle,
+      data: this.data2,
+      color: this.colorVerde
+    },
+    {
+      name: 'bar3',
+      type: 'bar',
+      stack: 'uno',
+      emphasis: this.emphasisStyle,
+      data: this.data3,
+      color: this.colorAmarillo
+    },
+    {
+      name: 'bar4',
+      type: 'bar',
+      stack: 'dos',
+      emphasis: this.emphasisStyle,
+      data: this.data4,
+      color: this.colorAzul
+    },
+    {
+      name: 'bar5',
+      type: 'bar',
+      stack: 'dos',
+      emphasis: this.emphasisStyle,
+      data: this.data5,
+      color: this.colorVerde
+    },
+    {
+      name: 'bar6',
+      type: 'bar',
+      stack: 'dos',
+      emphasis: this.emphasisStyle,
+      data: this.data6,
+      color: this.colorAmarillo
+    },
+    {
+      name: 'bar7',
+      type: 'bar',
+      stack: 'tres',
+      emphasis: this.emphasisStyle,
+      data: this.data7,
+      color: this.colorAzul
+    },
+    {
+      name: 'bar8',
+      type: 'bar',
+      stack: 'tres',
+      emphasis: this.emphasisStyle,
+      data: this.data8,
+      color: this.colorVerde
+    },
+    {
+      name: 'bar9',
+      type: 'bar',
+      stack: 'tres',
+      emphasis: this.emphasisStyle,
+      data: this.data9,
+      color: this.colorAmarillo
+    },
+    {
+      name: 'bar10',
+      type: 'bar',
+      stack: 'cuatro',
+      emphasis: this.emphasisStyle,
+      data: this.data10,
+      color: this.colorAzul
+    },
+    {
+      name: 'bar11',
+      type: 'bar',
+      stack: 'cuatro',
+      emphasis: this.emphasisStyle,
+      data: this.data11,
+      color: this.colorVerde
+    },
+    {
+      name: 'bar12',
+      type: 'bar',
+      stack: 'cuatro',
+      emphasis: this.emphasisStyle,
+      data: this.data12,
+      color: this.colorAmarillo
+    },
+    {
+      name: 'bar13',
+      type: 'bar',
+      stack: 'cinco',
+      emphasis: this.emphasisStyle,
+      data: this.data13,
+      color: this.colorAzul
+    },
+    {
+      name: 'bar14',
+      type: 'bar',
+      stack: 'cinco',
+      emphasis: this.emphasisStyle,
+      data: this.data14,
+      color: this.colorVerde
+    },
+    {
+      name: 'bar15',
+      type: 'bar',
+      stack: 'cinco',
+      emphasis: this.emphasisStyle,
+      data: this.data15,
+      color: this.colorAmarillo
+    },
+    {
+      name: 'bar16',
+      type: 'bar',
+      stack: 'seis',
+      emphasis: this.emphasisStyle,
+      data: this.data16,
+      color: this.colorAzul
+    },
+    {
+      name: 'bar17',
+      type: 'bar',
+      stack: 'seis',
+      emphasis: this.emphasisStyle,
+      data: this.data17,
+      color: this.colorVerde
+    },
+    {
+      name: 'bar18',
+      type: 'bar',
+      stack: 'seis',
+      emphasis: this.emphasisStyle,
+      data: this.data18,
+      color: this.colorAmarillo
+    },
+    {
+      name: 'bar19',
+      type: 'bar',
+      stack: 'siete',
+      emphasis: this.emphasisStyle,
+      data: this.data19,
+      color: this.colorAzul
+    },
+    {
+      name: 'bar20',
+      type: 'bar',
+      stack: 'siete',
+      emphasis: this.emphasisStyle,
+      data: this.data20,
+      color: this.colorVerde
+    },
+    {
+      name: 'bar21',
+      type: 'bar',
+      stack: 'siete',
+      emphasis: this.emphasisStyle,
+      data: this.data21,
+      color: this.colorAmarillo
+    },
+
+    // {
+    //   data: this.linea1,
+    //   type: 'line',
+    //   color: '#FF9F17'
+    // }
+    {
+      type: 'graph',
+      layout: 'none',
+      coordinateSystem: 'cartesian2d',
+      symbolSize: 5,
+      edgeSymbol: ['circle'],
+      edgeSymbolSize: [0, 0],
+      data: this.linea1,
+      links: this.link,
+      lineStyle: {
+              color: '#ffa015',
+              width:1,
+              opacity: 1
+
+      },
+      label: {
+          formatter: function(){
+              return '';
+          }
+      },
+      color: '#ffa015',
+    }
+
+  ]
+}
+
+
+
+  constructor() { 
+  for (var i = 0; i < 57; i++) {
+    this.data1.push(Math.floor(Math.random() * 1000000) + 500000);
+    this.data2.push(Math.floor(Math.random() * 1000000) + 500000);
+    this.data3.push(Math.floor(Math.random() * 1000000) + 500000);
+    this.data4.push(Math.floor(Math.random() * 1000000) + 500000);
+    this.data5.push(Math.floor(Math.random() * 1000000) + 500000);
+    this.data6.push(Math.floor(Math.random() * 1000000) + 500000);
+    this.data7.push(Math.floor(Math.random() * 1000000) + 500000);
+    this.data8.push(Math.floor(Math.random() * 1000000) + 500000);
+    this.data9.push(Math.floor(Math.random() * 1000000) + 500000);
+    this.data10.push(Math.floor(Math.random() * 1000000) + 500000);
+    this.data11.push(Math.floor(Math.random() * 1000000) + 500000);
+    this.data12.push(Math.floor(Math.random() * 1000000) + 500000);
+    this.data13.push(Math.floor(Math.random() * 1000000) + 500000);
+    this.data14.push(Math.floor(Math.random() * 1000000) + 500000);
+    this.data15.push(Math.floor(Math.random() * 1000000) + 500000);
+    this.data16.push(Math.floor(Math.random() * 1000000) + 500000);
+    this.data17.push(Math.floor(Math.random() * 1000000) + 500000);
+    this.data18.push(Math.floor(Math.random() * 1000000) + 500000);
+    this.data19.push(Math.floor(Math.random() * 1000000) + 500000);
+    this.data20.push(Math.floor(Math.random() * 1000000) + 500000);
+    this.data21.push(Math.floor(Math.random() * 1000000) + 500000);
+    this.linea1.push(Math.floor(Math.random() * 1000000) + 500000);
+  }
+
+  // for (let index = 0; index < 40; index++) {
+  //   const element = array[index];
+    
+  // }
+
+  for (let index = 7; index < 53; index++) {
+    this.xAxisData.push('Sem ' + index);
+  }
+
+  this.link = this.linea1.map(function (item, i) {
+    return {
+        source: i,
+        target: i + 1
+    };
+  });
+
+  console.log(this.link);
+  
+  }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/components/inventario-detalle/inventario-detalle.component.ts
+++ src/app/components/inventario-detalle/inventario-detalle.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { EChartsOption } from 'echarts';
+import * as am4core from "@amcharts/amcharts4/core";
+import * as am4charts from "@amcharts/amcharts4/charts";
+import am4themes_animated from "@amcharts/amcharts4/themes/animated";
 
 @Component({
   selector: 'app-inventario-detalle',
@@ -7,84 +9,232 @@ import { EChartsOption } from 'echarts';
 })
 export class InventarioDetalleComponent implements OnInit {
 
-xAxisData = [];
-data1 = [];
-data2 = [];
-data3 = [];
-data4 = [];
-
-emphasisStyle = {
-    itemStyle: {
-        shadowBlur: 10,
-        shadowColor: 'rgba(0,0,0,0.3)'
-    }
-};
-
-chartOption: EChartsOption = {
-  legend: {
-    data: ['bar', 'bar2', 'bar3', 'bar4'],
-    left: '10%'
-  },
-  brush: {
-    toolbox: ['rect', 'polygon', 'lineX', 'lineY', 'keep', 'clear'],
-    xAxisIndex: 0
-  },
-  tooltip: {},
-  xAxis: {
-    data: this.xAxisData,
-    name: 'X Axis',
-    axisLine: {onZero: true},
-    splitLine: {show: false},
-    splitArea: {show: false}
-  },
-  yAxis: {},
-  grid: {
-    bottom: 100
-  },
-  series: [
-    {
-      name: 'bar',
-      type: 'bar',
-      stack: 'one',
-      emphasis: this.emphasisStyle,
-      data: this.data1
-    },
-    {
-      name: 'bar2',
-      type: 'bar',
-      stack: 'one',
-      emphasis: this.emphasisStyle,
-      data: this.data2
-    },
-    {
-      name: 'bar3',
-      type: 'bar',
-      stack: 'two',
-      emphasis: this.emphasisStyle,
-      data: this.data3
-    },
-    {
-      name: 'bar4',
-      type: 'bar',
-      stack: 'two',
-      emphasis: this.emphasisStyle,
-      data: this.data4
-    }
-  ]
-}
 
   constructor() { 
-  for (var i = 0; i < 10; i++) {
-    this.xAxisData.push('Class' + i);
-    this.data1.push((Math.random() * 2).toFixed(2));
-    this.data2.push((Math.random() * 5).toFixed(2));
-    this.data3.push((Math.random() + 0.3).toFixed(2));
-    this.data4.push(-Math.random().toFixed(2));
-  }
   
+
+    
   }
 
   ngOnInit(): void {
+    am4core.useTheme(am4themes_animated);
+    // Themes end
+    
+    // Create chart instance
+    let chart = am4core.create("chartdiv", am4charts.XYChart);
+    
+    chart.colors.step = 2;
+    chart.maskBullets = false;
+    
+    // Add data
+    chart.data = [{
+        "date": "2012-01-01",
+        "distance": 227,
+        "townName": "New York",
+        "townName2": "New York",
+        "townSize": 12,
+        "latitude": 40.71,
+        "duration": 408
+    }, {
+        "date": "2012-01-02",
+        "distance": 371,
+        "townName": "Washington",
+        "townSize": 7,
+        "latitude": 38.89,
+        "duration": 482
+    }, {
+        "date": "2012-01-03",
+        "distance": 433,
+        "townName": "Wilmington",
+        "townSize": 3,
+        "latitude": 34.22,
+        "duration": 562
+    }, {
+        "date": "2012-01-04",
+        "distance": 345,
+        "townName": "Jacksonville",
+        "townSize": 3.5,
+        "latitude": 30.35,
+        "duration": 379
+    }, {
+        "date": "2012-01-05",
+        "distance": 480,
+        "townName": "Miami",
+        "townName2": "Miami",
+        "townSize": 5,
+        "latitude": 25.83,
+        "duration": 501
+    }, {
+        "date": "2012-01-06",
+        "distance": 386,
+        "townName": "Tallahassee",
+        "townSize": 3.5,
+        "latitude": 30.46,
+        "duration": 443
+    }, {
+        "date": "2012-01-07",
+        "distance": 348,
+        "townName": "New Orleans",
+        "townSize": 5,
+        "latitude": 29.94,
+        "duration": 405
+    }, {
+        "date": "2012-01-08",
+        "distance": 238,
+        "townName": "Houston",
+        "townName2": "Houston",
+        "townSize": 8,
+        "latitude": 29.76,
+        "duration": 309
+    }, {
+        "date": "2012-01-09",
+        "distance": 218,
+        "townName": "Dalas",
+        "townSize": 8,
+        "latitude": 32.8,
+        "duration": 287
+    }, {
+        "date": "2012-01-10",
+        "distance": 349,
+        "townName": "Oklahoma City",
+        "townSize": 5,
+        "latitude": 35.49,
+        "duration": 485
+    }, {
+        "date": "2012-01-11",
+        "distance": 603,
+        "townName": "Kansas City",
+        "townSize": 5,
+        "latitude": 39.1,
+        "duration": 890
+    }, {
+        "date": "2012-01-12",
+        "distance": 534,
+        "townName": "Denver",
+        "townName2": "Denver",
+        "townSize": 9,
+        "latitude": 39.74,
+        "duration": 810
+    }, {
+        "date": "2012-01-13",
+        "townName": "Salt Lake City",
+        "townSize": 6,
+        "distance": 425,
+        "duration": 670,
+        "latitude": 40.75,
+        "dashLength": 8,
+        "alpha": 0.4
+    }, {
+        "date": "2012-01-14",
+        "latitude": 36.1,
+        "duration": 470,
+        "townName": "Las Vegas",
+        "townName2": "Las Vegas"
+    }, {
+        "date": "2012-01-15"
+    }, {
+        "date": "2012-01-16"
+    }, {
+        "date": "2012-01-17"
+    }];
+    
+    // Create axes
+    let dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+    dateAxis.renderer.grid.template.location = 0;
+    dateAxis.renderer.minGridDistance = 50;
+    dateAxis.renderer.grid.template.disabled = true;
+    dateAxis.renderer.fullWidthTooltip = true;
+    
+    let distanceAxis = chart.yAxes.push(new am4charts.ValueAxis());
+    distanceAxis.title.text = "Distance";
+    //distanceAxis.renderer.grid.template.disabled = true;
+    
+    let durationAxis = chart.yAxes.push(new am4charts.DurationAxis());
+    durationAxis.title.text = "Duration";
+    durationAxis.baseUnit = "minute";
+    //durationAxis.renderer.grid.template.disabled = true;
+    durationAxis.renderer.opposite = true;
+    durationAxis.syncWithAxis = distanceAxis;
+    
+    durationAxis.durationFormatter.durationFormat = "hh'h' mm'min'";
+    
+    let latitudeAxis = chart.yAxes.push(new am4charts.ValueAxis());
+    latitudeAxis.renderer.grid.template.disabled = true;
+    latitudeAxis.renderer.labels.template.disabled = true;
+    latitudeAxis.syncWithAxis = distanceAxis;
+    
+    // Create series
+    let distanceSeries = chart.series.push(new am4charts.ColumnSeries());
+    distanceSeries.dataFields.valueY = "distance";
+    distanceSeries.dataFields.dateX = "date";
+    distanceSeries.yAxis = distanceAxis;
+    distanceSeries.tooltipText = "{valueY} miles";
+    distanceSeries.name = "Distance";
+    distanceSeries.columns.template.fillOpacity = 0.7;
+    distanceSeries.columns.template.propertyFields.strokeDasharray = "dashLength";
+    distanceSeries.columns.template.propertyFields.fillOpacity = "alpha";
+    distanceSeries.showOnInit = true;
+    
+    let distanceState = distanceSeries.columns.template.states.create("hover");
+    distanceState.properties.fillOpacity = 0.9;
+    
+    let durationSeries = chart.series.push(new am4charts.LineSeries());
+    durationSeries.dataFields.valueY = "duration";
+    durationSeries.dataFields.dateX = "date";
+    durationSeries.yAxis = durationAxis;
+    durationSeries.name = "Duration";
+    durationSeries.strokeWidth = 2;
+    durationSeries.propertyFields.strokeDasharray = "dashLength";
+    durationSeries.tooltipText = "{valueY.formatDuration()}";
+    durationSeries.showOnInit = true;
+    
+    let durationBullet = durationSeries.bullets.push(new am4charts.Bullet());
+    let durationRectangle = durationBullet.createChild(am4core.Rectangle);
+    durationBullet.horizontalCenter = "middle";
+    durationBullet.verticalCenter = "middle";
+    durationBullet.width = 7;
+    durationBullet.height = 7;
+    durationRectangle.width = 7;
+    durationRectangle.height = 7;
+    
+    let durationState = durationBullet.states.create("hover");
+    durationState.properties.scale = 1.2;
+    
+    let latitudeSeries = chart.series.push(new am4charts.LineSeries());
+    latitudeSeries.dataFields.valueY = "latitude";
+    latitudeSeries.dataFields.dateX = "date";
+    latitudeSeries.yAxis = latitudeAxis;
+    latitudeSeries.name = "Duration";
+    latitudeSeries.strokeWidth = 2;
+    latitudeSeries.propertyFields.strokeDasharray = "dashLength";
+    latitudeSeries.tooltipText = "Latitude: {valueY} ({townName})";
+    latitudeSeries.showOnInit = true;
+    
+    let latitudeBullet = latitudeSeries.bullets.push(new am4charts.CircleBullet());
+    latitudeBullet.circle.fill = am4core.color("#fff");
+    latitudeBullet.circle.strokeWidth = 2;
+    latitudeBullet.circle.propertyFields.radius = "townSize";
+    
+    let latitudeState = latitudeBullet.states.create("hover");
+    latitudeState.properties.scale = 1.2;
+    
+    let latitudeLabel = latitudeSeries.bullets.push(new am4charts.LabelBullet());
+    latitudeLabel.label.text = "{townName2}";
+    latitudeLabel.label.horizontalCenter = "left";
+    latitudeLabel.label.dx = 14;
+    
+    // Add legend
+    chart.legend = new am4charts.Legend();
+    
+    // Add cursor
+    chart.cursor = new am4charts.XYCursor();
+    chart.cursor.fullWidthLineX = true;
+    chart.cursor.xAxis = dateAxis;
+    chart.cursor.lineX.strokeOpacity = 0;
+    chart.cursor.lineX.fill = am4core.color("#000");
+    chart.cursor.lineX.fillOpacity = 0.1;
   }
+
+  
 
 }
